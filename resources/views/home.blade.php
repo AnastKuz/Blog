0841cdff1b92a8a7f5800fa412ce35e73ddb53a6
Anastasia Kuzmenko <knigoed92@gmail.com>
2020-03-23 16:09:43 +0300

    making posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,28 @@
 
 @section('content')
 
+    <div class="container">
+{{dd($posts)}}
+        @if($posts->count() > 0)
+            @foreach($posts as $post)
+                <div class="card my-5">
+                    <div class="card-header">
+                        <h2>{{$posts->title}}</h2>
+                    </div>
+                    <div class="card-body">
+                        <p>{{$posts->created_at}}</p>
+                        <img src="{{$posts->photo}}" alt="image">
+                        <p class="card-text">{{$posts->body}}</p>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            <div class="row">
+                <div class="col">
+            <h4 class="text-center">No posts found!</h4>
+                </div>
+            </div>
+        @endif
+
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
+
+Route::resource('posts', 'PostsController');
